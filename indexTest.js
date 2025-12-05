3f9de7ebde213ac487ed30e2fc007036de35ab27
IM-IGTM <54503554+IM-IGTM@users.noreply.github.com>
2025-12-05 17:33:44 +0900

    251205 js 수정
    251205 js 수정

--- indexTest.js
+++ indexTest.js
@@ -29,3 +29,19 @@ function preStartTest() {
   const encodedName = encodeURIComponent(name);
   window.location.href = `preStarter/preStarter.html?studentName=${encodedName}`;
 }
+
+function basic() {
+  // 입력한 이름 가져오기
+  const input = document.getElementById("studentName");
+  const name = input.value.trim();
+
+  if (!name) {
+    alert("이름을 입력해 주세요.");
+    input.focus();
+    return;
+  }
+
+  // URL에 이름을 쿼리스트링으로 붙여서 이동
+  const encodedName = encodeURIComponent(name);
+  window.location.href = `basic/basicTest.html?studentName=${encodedName}`;
+}
