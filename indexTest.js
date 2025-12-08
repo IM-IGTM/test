7da140133a5a992477d340fbaace81bfeb4172a5
IM-IGTM <54503554+IM-IGTM@users.noreply.github.com>
2025-12-08 22:56:44 +0900

    251208 index upload
    251208 index upload

--- indexTest.js
+++ indexTest.js
@@ -29,7 +29,21 @@ function preStartTest() {
   const encodedName = encodeURIComponent(name);
   window.location.href = `preStarter/preStarter.html?studentName=${encodedName}`;
 }
+function startTest() {
+  // 입력한 이름 가져오기
+  const input = document.getElementById("studentName");
+  const name = input.value.trim();
 
+  if (!name) {
+    alert("이름을 입력해 주세요.");
+    input.focus();
+    return;
+  }
+
+  // URL에 이름을 쿼리스트링으로 붙여서 이동
+  const encodedName = encodeURIComponent(name);
+  window.location.href = `starter/starter.html?studentName=${encodedName}`;
+}
 function basic() {
   // 입력한 이름 가져오기
   const input = document.getElementById("studentName");
